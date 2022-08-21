c2109a67d0572511abfc78076fb1c9a61f33f77f
Brijesh99094 <prajapatibrijesh655@gmail.com>
2022-08-21 14:23:47 +0530

    first faild test

---
+++ string_calculator.py
@@ -0,0 +1,4 @@
+
+
+class StringCalculator:
+    pass
---
+++ test_string_calculator.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import unittest
+from string_calculator import StringCalculator
+
+
+class StringCalculatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.sc = StringCalculator()
+
+    def test_return_zero_if_string_is_empty(self):
+        self.assertEqual(self.sc.add(""), 6, "Must return 0 for empty string")
