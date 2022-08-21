efebe689f3164643099be0ffa4d651feb4bc8e6a
Brijesh99094 <prajapatibrijesh655@gmail.com>
2022-08-21 22:39:10 +0530

    Refactor the code using extract method

--- string_calculator.py
+++ string_calculator.py
@@ -22,6 +22,7 @@ class StringCalculator:
             print("Invalid String")
         return string, delimiter
 
+
     def add(self, string_number):
         if len(string_number.strip()) == 0:
             return 0
@@ -31,29 +32,36 @@ class StringCalculator:
                 string_number)
             lst = list()
             negative_values = list()
-            for i in re.split(delimiter, string_number):
-                if not i.isdigit():
-                    if len(i) == 1:
-                        num = ord(i) - 96
-                        lst.append(num)
-                    else:
-                        #raise ValueError("Negative not allowed")
-                        negative_values.append(int(i))
-                elif int(i) < 0:
-                    negative_values.append(int(i))
-                    #raise ValueError("Negative value not allowed")
-                elif int(i) > 1000:
-                    continue
-                else:
-                    lst.append(int(i))
+            self.get_valid_or_negative_numbers(string_number, delimiter, lst, negative_values)
 
-            if (negative_values):
-                s = ""
-                for i in negative_values:
-                    s += (str(i)+',')
-                s = s[0:len(s)-1]
-                raise ValueError(s)
+            self.raise_exception_for_negative_values(negative_values)
         return sum(lst)
+
+
+    def get_valid_or_negative_numbers(self, string_number, delimiter, lst, negative_values):
+        for i in re.split(delimiter, string_number):
+            if not i.isdigit():
+                if len(i) == 1:
+                    num = ord(i) - 96
+                    lst.append(num)
+                else:
+                        #raise ValueError("Negative not allowed")
+                    negative_values.append(int(i))
+            elif int(i) < 0:
+                negative_values.append(int(i))
+                    #raise ValueError("Negative value not allowed")
+            elif int(i) > 1000:
+                continue
+            else:
+                lst.append(int(i))
+
+    def raise_exception_for_negative_values(self, negative_values):
+        if (negative_values):
+            s = ""
+            for i in negative_values:
+                s += (str(i)+',')
+            s = s[0:len(s)-1]
+            raise ValueError(s)
 
 
 if __name__ == "__main__":
